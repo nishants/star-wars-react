ed05b0d8352606d70f2e67ddb5d6bee8be9e2484
nishant <nishant.singh87@gmail.com>
2018-02-25 02:44:27 +0530

    Show vehicle menu after selecting planets.

--- src/app/missions-home/create-mission/create-mission-component.js
+++ src/app/missions-home/create-mission/create-mission-component.js
@@ -3,21 +3,28 @@ import {connect} from 'react-redux';
 import SelectPlanets from './select-planets/select-planets-component';
 import {CREATE_NEW_MISSION, CANCEL_CREATING_MISSION, selectPlanet} from './create-mission-actions'
 
-const MissionsWidget = ({menu, createMission, cancelCreatingMission, selectPlanet})=> (
-    <div id='missions-widget' className={menu.showMenu ? 'create': ''}>
-      <div className='backdrop' onClick={cancelCreatingMission}></div>
-      <div className='actions'>
-        <button className='create' onClick={createMission}>Create a mission</button>
-        <button className='cancel' onClick={cancelCreatingMission}>Cancel</button>
-      </div>
-      <div className='menu'>
-        <div className='sub-menu select-planet'>
-          <label> 1. Select Planet</label>
-          <SelectPlanets onSelect={selectPlanet}/>
+const MissionsWidget = ({menu, createMission, cancelCreatingMission, selectPlanet})=> {
+  const widgetState = `${menu.showMenu ? 'create' : ''} ${menu.showVehicleMenu ? 'vehicle' : ''} `;
+  return (
+      <div id='missions-widget' className={widgetState}>
+        <div className='backdrop' onClick={cancelCreatingMission}></div>
+        <div className='actions'>
+          <button className='create' onClick={createMission}>Create a mission</button>
+          <button className='cancel' onClick={cancelCreatingMission}>Cancel</button>
+        </div>
+        <div className='menu'>
+          <div className='sub-menu select-planet'>
+            <label> 1. Select Planet</label>
+            <SelectPlanets onSelect={selectPlanet}/>
+          </div>
+          <div className='sub-menu select-vehicle'>
+            <label> 2. Select Vehicle</label>
+            <SelectPlanets onSelect={selectPlanet}/>
+          </div>
         </div>
       </div>
-    </div>
-);
+  )
+};
 
 const mapStateToProps = ({createMission})=> ({menu: createMission});
 
