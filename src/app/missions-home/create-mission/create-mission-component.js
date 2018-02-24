9db75d3be720f242ad4075ca638408b35e838d30
nishant <nishant.singh87@gmail.com>
2018-02-24 23:00:52 +0530

    Add missions widget and create button.

---
+++ src/app/missions-home/create-mission/create-mission-component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MissionsWidget = ()=> (
+    <div id='missions-widget'>
+      <div className='backdrop'></div>
+      <div className='actions'>
+        <button className='create'>Create</button>
+      </div>
+    </div>
+);
+
+export default MissionsWidget;
--- src/app/missions-home/mission-home-component.js
+++ src/app/missions-home/mission-home-component.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HelpButton from './help-button/help-buttton-component';
+import CreateMissions from './create-mission/create-mission-component';
 
 const MissionsHome = () => {
   return (
       <div id='missions-home'>
         <HelpButton/>
+        <CreateMissions/>
       </div>);
 }
 
