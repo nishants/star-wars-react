87f367af7e5e98a3e33092834aec4c4929c5cbd8
nishant <nishant.singh87@gmail.com>
2018-02-25 01:14:11 +0530

    Add redux thunk and axios.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './app/app';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './app/reducers';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
-    <Provider store={createStore(rootReducer)}>
+    <Provider store={createStore(rootReducer, applyMiddleware(thunk))}>
       <App/>
     </Provider>,
     document.getElementById('root'));
+
 registerServiceWorker();
